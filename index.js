8728211d12dc2db7ee63e725d234aac92bb517c8
Marcin Arkan <marcinxe@gmail.com>
2021-12-08 20:24:00 +0100

    Figure out the addresses of the LEDs

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 
-var packetsData = require("./packets");
+var ledsData = require("./leds");
 
 // console.log(packets)
 
 var HID = require('node-hid');
-Array.prototype.zeroFill = function (len) {
-    for (var i = this.length; i < len; i++) {
-        this[i] = 0;
-    }
-    return this;
-};
 console.log("im alive")
 var devices = HID.devices()
 var deviceInfo = devices.find(function (d) {
@@ -20,12 +14,25 @@ if (deviceInfo) {
     var device = new HID.HID(deviceInfo.path)
     console.log({ deviceInfo, device })
     // ... use device
-    for (let i = 0; i <= 200; i++) {
-        console.log(`trying write ${i}/200`)
-        let values = packetsData.packets[i % 4]
-    console.log("values to write", values, values.length)
-    device.sendFeatureReport(values)
-        console.log(`written data ${i}/200`, values)
-    }
+
+
+
+
+    var result = ledsData.leds.reduce((resultArray, item, index) => {
+        const chunkIndex = Math.floor(index / 180)
+        if (!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = [] // start a new chunk
+        }
+
+        resultArray[chunkIndex].push(item)
+
+        return resultArray
+    }, [])
+console.log({result})
+    result.forEach(chunk => {
+        console.log(`trying write ${chunk.length}`)
+
+        device.sendFeatureReport([0x07, 0xa1, 0x2f, 0x00, ...chunk])
+    })
 }
 
